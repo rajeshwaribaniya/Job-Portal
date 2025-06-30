485a678d1e69ee6b09bc413daa6db50d972cda75
sinapradip <sinapradip@gmail.com>
2025-06-30 15:36:04 +0545

    extract token from authorization from

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,35 +1,39 @@
 import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
-    
-    try {
-        const token = req.cookies.token;
+  try {
+    const token = req.cookies.token;
 
-        if(!token) {
-            return res.status(401).json({
-                message: "User not authenticated",
-                success: false
-            })
-        }
-
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
-
-        if(!decode) {
-            return res.status(401).json({
-                message: "User not authenticated",
-                success: false
-            }) 
-        }
-
-        req.id = decode.userId;
-        next();
-
-    } catch (error) {
-        return res.status(500).json({
-            message: 'An error occured. User not authenticated'
-       })
+    if (!token) {
+      return res.status(401).json({
+        message: "User not authenticated",
+        success: false,
+      });
+    }
+    // If token is not found in cookies, check the Authorization header
+    if (!token) {
+      const authHeader = req.headers.authorization;
+      if (authHeader && authHeader.startsWith("Bearer ")) {
+        token = authHeader.substring(7); // Remove 'Bearer ' prefix
+      }
     }
 
-}
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decode) {
+      return res.status(401).json({
+        message: "User not authenticated",
+        success: false,
+      });
+    }
+
+    req.id = decode.userId;
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      message: "An error occured. User not authenticated",
+    });
+  }
+};
 
 export default isAuthenticated;
